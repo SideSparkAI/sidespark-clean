c0e7669dd938ebf8097930969907303703706ad5
Brian Toback <sidesparkai@gmail.com>
2025-08-15 12:03:52 +0700

    Switch to Webpack and add @ alias for build compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@ import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turbo: {
+      loaders: {
+        disable: true
+      }
+    }
+  },
   webpack(config) {
     config.resolve.alias['@'] = path.resolve(process.cwd(), 'src');
     return config;
